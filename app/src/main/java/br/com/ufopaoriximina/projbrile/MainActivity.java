640e65644a09e456876c4f8acb164db609c3d401
Samuel <samuelamorim865@gmail.com>
2020-07-02 11:28:21 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ufopaoriximina/projbrile/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.ufopaoriximina.projbrile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ufopaoriximina.projbrile.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
