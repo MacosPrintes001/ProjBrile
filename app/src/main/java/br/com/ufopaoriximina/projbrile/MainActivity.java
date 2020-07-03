90217508276b4de74946f4a77c575ba76a795592
MacosPrintes001 <mvprintes@gmail.com>
2020-07-02 23:19:48 -0300

    Arquivo Tradução Braile

--- app/src/main/java/br/com/ufopaoriximina/projbrile/MainActivity.java
+++ app/src/main/java/br/com/ufopaoriximina/projbrile/MainActivity.java
@@ -1,13 +1,296 @@
 package br.com.ufopaoriximina.projbrile;
-import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import br.com.ufopaoriximina.projbrile.R;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView mImageView;
+    Button mGalerryBtn, mApllyBtn;
+    Bitmap grayBitMap, imgBitMap;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OpenCVLoader.initDebug();
+
+        mImageView = findViewById(R.id.imageView);
+
+        mGalerryBtn = findViewById(R.id.btnChooseImg);
+        mGalerryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                        //permission not granted, request it
+                        String[] permissions ={Manifest.permission.READ_EXTERNAL_STORAGE};
+
+                        //Show pop in runtime
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    }
+                    else{
+                        //Permission is already granted
+                        pickImageGallery();
+                    }
+                }
+                else{
+                    //System OS is less them MARSHMALLOW
+                    pickImageGallery();
+                }
+            }
+        });
+
+        //cria evento de click no botão "APLLY"
+        mApllyBtn = findViewById(R.id.grayBtn);
+        mApllyBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                try {
+                    cornvertToGray();
+                }
+                catch (Exception o){
+                    o.getSuppressed();
+                }
+            }
+        });
+
+    }
+
+    public void pickImageGallery(){
+
+        // Intent to pick up Image
+        Intent  intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    //handle result of runtime permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permission was granted
+                pickImageGallery();
+            } else {
+                //permission was Denied
+                Toast.makeText(this, "Permission denied, pleace Allow the permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override //carrega a imagem para a tela
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+
+
+            assert data != null;
+
+            //pega a imagem e coloca em "SelectedImage"
+            Uri selectedImage = data.getData();
+
+            try {
+
+                imgBitMap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+            }catch (IOException e){
+
+                e.printStackTrace();
+            }
+
+            mImageView.setImageBitmap(imgBitMap);
+        }
+
+    }
+
+    public void cornvertToGray(){
+        if (imgBitMap == null){ //se nenhuma imagem selecionada
+
+            Toast.makeText(this, "ImageView is emprety, pleace select an image", Toast.LENGTH_SHORT ).show();
+
+        }else {// tem imagem hora de trabalhar
+            Mat Rgba = new Mat();
+            Mat grayMat = new Mat();
+
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inDither = false;
+            o.inSampleSize = 4;
+
+            int width = imgBitMap.getWidth();
+            int height = imgBitMap.getHeight();
+
+            grayBitMap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+            //BitMap to mat
+            Utils.bitmapToMat(imgBitMap, Rgba);
+
+            //Gray Scale
+            Imgproc.cvtColor(Rgba, grayMat, Imgproc.COLOR_RGB2GRAY);
+
+            //ThresHold(Limiarização)
+            Imgproc.threshold(grayMat, grayMat, 233, 255, Imgproc.THRESH_BINARY_INV);
+
+            //Median Filter(Filtro de Mediana/Morfologia)
+            Imgproc.medianBlur(grayMat, grayMat, 3);
+
+            //Dilate
+            Imgproc.dilate(grayMat, grayMat, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(1, 2)));
+
+            //Erode
+            Imgproc.erode(grayMat, grayMat, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(1, 1)));
+
+            //Toast.makeText(this, "The image was loaded", Toast.LENGTH_SHORT ).show();
+
+            // find contours (Acha o contorno dos circulos)
+            List<MatOfPoint> countours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(grayMat, countours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            //Find centroids (acha p centro de cada circulo e coloca em X(coluna) Y(Linha))
+            ArrayList<Integer> arrayX = new ArrayList<>();
+            ArrayList<Integer> arrayY = new ArrayList<>();
+            List<Moments> mu = new ArrayList<>();
+
+            for (int i = 0; i < countours.size(); i++) {
+
+                mu.add(i, Imgproc.moments(countours.get(i), true));
+                Moments p = mu.get(i);
+                int x = (int) (p.get_m10() / p.get_m00());
+                int y = (int) (p.get_m01() / p.get_m00());
+                arrayX.add(x);
+                arrayY.add(y);
+            }
+
+            int[][] centroids = new int[arrayX.size()][2];
+            for (int i = 0; i < arrayX.size(); i++){
+                centroids[i][0] = arrayX.get(i);
+                centroids[i][1] = arrayY.get(i);
+            }
+
+            //draw lines
+            Collections.sort(arrayX);
+            Collections.sort(arrayY);
+
+            Set<Integer> xSemRepetir = new LinkedHashSet<>(arrayX);
+
+            Set<Integer> ySemRepetir = new LinkedHashSet<>(arrayY);
+
+            Integer[] arrayXSemRep = new Integer[xSemRepetir.size()];
+            xSemRepetir.toArray(arrayXSemRep);
+
+            Integer[] arrayYSemRep = new Integer[ySemRepetir.size()];
+            ySemRepetir.toArray(arrayYSemRep);
+
+            ArrayList<Integer> index_col = new ArrayList<>();
+            index_col.add(0);
+            Integer contadorAntesX = 0;
+            for (Integer integer : arrayXSemRep) {
+                if (integer > contadorAntesX) {
+                    Imgproc.line(grayMat, new Point(integer + 6, 0), new Point(integer + 6, 1800), new Scalar(130, 0, 0)); //fazer colunas
+                    contadorAntesX = integer + 6;
+                    index_col.add(contadorAntesX);
+                }
+            }
+
+            ArrayList<Integer> index_lin = new ArrayList<>();
+            index_lin.add(0);
+            Integer contadorAntesY = 0;
+            for (Integer integer : arrayYSemRep) {
+                if (integer > contadorAntesY) {
+                    Imgproc.line(grayMat, new Point(0, integer + 6), new Point(900, integer + 6), new Scalar(130, 0, 0)); //fazer linhas
+                    contadorAntesY = integer + 6;
+                    index_lin.add(contadorAntesY);
+                }
+            }
+
+            //Recognition of word
+            String flag="0";
+            for(int i=0; i<=(index_lin.size()-3); i=i+3) {
+                for (int j = 0; j < (index_col.size() - 2); j += 2) {
+
+                    int[] Vlinha = new int[4];
+                    int[] Vcoluna = new int[3];
+                    int[] Vletra = {0, 0, 0, 0, 0, 0};
+
+                    int m = 0, cnt = 0;
+                    for(int p = i; p<=(i+3); p++) {
+                        Vlinha[m] = index_lin.get(p);
+                        m++;
+                    }
+                    int n=0;
+                    for(int t = j; t<= (j+2); t++) {
+                        Vcoluna[n] = index_col.get(t);
+                        n++;
+                    }
+
+                    for(m=0; m<(Vcoluna.length)-1; m++) {
+                        for(n=0; n<(Vlinha.length)-1; n++) {
+
+                            //pointCentroids Centroids = new pointCentroids();
+                            //int [][] centroids = Centroids.Centroids();
+
+                            for(int k=0; k<centroids.length; k++ ) {
+                                if(centroids[k][0] >= Vcoluna[m] && centroids[k][0] <= Vcoluna[m+1] && centroids[k][1] >= Vlinha[n] && centroids[k][1] <= Vlinha[n+1]) {                                    //centroids[0][k] >= Vcoluna[m] && centroids[0][k] <= Vcoluna[m+1] && centroids[1][k] >= Vlinha[n] && centroids[1][k] <= Vlinha[n+1]){
+                                    Vletra[cnt] = 1;
+                                }
+                            }
+                            cnt = cnt+1;
+                        }
+                    }
+
+                    bdLetra carac = new bdLetra();
+                    String[] letra = carac.letraBD(Vletra, flag);
+                    System.out.println(letra[0]);
+                    flag = letra[1];
+
+                }
+            }
+
+            Utils.matToBitmap(grayMat, grayBitMap);
+            //set to the Imageview
+            mImageView.setImageBitmap(grayBitMap);
+        }
     }
 }
---
+++ app/src/main/java/br/com/ufopaoriximina/projbrile/bdLetra.java
@@ -0,0 +1,326 @@
+package br.com.ufopaoriximina.projbrile;
+
+import java.util.Arrays;
+
+public class bdLetra {
+    public static String[] letraBD(int[] snap, String flags) {
+        int flag = Integer.parseInt(flags);
+
+        int [] a = {1, 0, 0, 0, 0, 0};
+        int [] b = {1, 1, 0, 0, 0, 0};
+        int [] c = {1, 0, 0, 1, 0, 0};
+        int [] d = {1, 0, 0, 1, 1, 0};
+        int [] e = {1, 0, 0, 0, 1, 0};
+        int [] f = {1, 1, 0, 1, 0, 0};
+        int [] g = {1, 1, 0, 1, 1, 0};
+        int [] h = {1, 1, 0, 0, 1, 0};
+        int [] i = {0, 1, 0, 1, 0, 0};
+        int [] j = {0, 1, 0, 1, 1, 0};
+        int [] k = {1, 0, 1, 0, 0, 0};
+        int [] l = {1, 1, 1, 0, 0, 0};
+        int [] m = {1, 0, 1, 1, 0, 0};
+        int [] n = {1, 0, 1, 1, 1, 0};
+        int [] o = {1, 0, 1, 0, 1, 0};
+        int [] p = {1, 1, 1, 1, 0, 0};
+        int [] q = {1, 1, 1, 1, 1, 0};
+        int [] r = {1, 1, 1, 0, 1, 0};
+        int [] s = {0, 1, 1, 1, 0, 0};
+        int [] t = {0, 1, 1, 1, 1, 0};
+        int [] u = {1, 0, 1, 0, 0, 1};
+        int [] v = {1, 1, 1, 0, 0, 1};
+        int [] w = {0, 1, 0, 1, 1, 1};
+        int [] x = {1, 0, 1, 1, 0, 1};
+        int [] y = {1, 0, 1, 1, 1, 1};
+        int [] z = {1, 0, 1, 0, 1, 1};
+        int [] cedilha ={1, 1, 1, 1, 0, 1};
+        int [] espaco = {0, 0, 0, 0, 0, 0};
+        int [] ag = {1, 1, 1, 0, 1, 1};
+        int [] eg = {1, 1, 1, 1, 1, 1};
+        int [] ig = {0, 0, 1, 1, 0, 0};
+        int [] og = {0, 0, 1, 1, 0, 1};
+        int [] ug = {0, 1, 1, 1, 1, 1};
+        int [] acr = {1, 1, 0, 1, 0, 1};
+        int [] ecr = {0, 1, 1, 1, 0, 1};
+        int [] icr = {1, 0, 0, 1, 0, 1};
+        int [] ocr = {0, 1, 0, 1, 1, 1};
+        int [] ucr = {1, 0, 0, 0, 1, 1};
+        int [] ac = {1, 0, 0, 0, 0, 1};
+        int [] ec = {1, 1, 0, 0, 0, 1};
+        int [] oc = {1, 0, 0, 1, 1, 1};
+        int [] at = {0, 0, 1, 1, 1, 0};
+        int [] ot = {0, 1, 0, 1, 0, 1};
+        int [] virg  = {0, 1, 0, 0, 0, 0};
+        int [] doisp = {0, 1, 0, 0, 1, 0};
+        int [] ponto = {0, 0, 1, 0, 0, 0};
+        int [] excla = {0, 1, 0, 1, 0, 1};
+        int [] inte = {0, 1, 0, 0, 0, 1};
+        int [] pontvirg = {0, 1, 1, 0, 0, 0};
+        int [] maiuscula = {0, 0, 0, 1, 0, 1};
+
+        int [] abP = {1, 1, 0, 0, 0, 1};
+        int [] feP = {0, 0, 1, 1, 1, 0};
+        int [] mais = {0, 1, 1, 0, 1, 0};
+        int [] hif = {0, 0, 1, 0, 0, 1};
+        int [] aster = {0, 1, 1, 0, 0, 1};
+        int [] divisao = {0, 1, 0, 0, 1, 1};
+        int [] igual = {0, 1, 1, 0, 1, 1};
+
+        String[] letter = new String [2];
+
+
+
+        if(Arrays.equals(a,snap)){
+            if (flag==1) {
+                letter[0]="A";
+                flag = 0;
+            }else {
+                letter[0]="a";}
+
+        }else if(Arrays.equals(b,snap)){
+            if (flag==1) {
+                letter[0]="B";
+                flag=0;
+            }else {
+                letter[0]="b";}
+
+        }else if(Arrays.equals(c,snap)){
+            if (flag==1){
+                letter[0]="C";
+                flag=0;
+            }else
+                letter[0]="c";
+
+        }else if(Arrays.equals(d, snap)){
+            if (flag==1){
+                letter[0]="D";
+                flag=0;
+            }else
+                letter[0]="d";
+
+        }else if(Arrays.equals(e,snap)){
+            if (flag==1){
+                letter[0]="E";
+                flag=0;
+            }else
+                letter[0]="e";
+
+        }else if(Arrays.equals(f, snap)){
+            if (flag==1){
+                letter[0]="F";
+                flag=0;
+            }else
+                letter[0]="f";
+
+        }else if(Arrays.equals(g, snap)){
+            if (flag==1){
+                letter[0]="G";
+                flag=0;
+            }else
+                letter[0]="g";
+
+        }else if(Arrays.equals(h,snap)){
+            if (flag==1){
+                letter[0]="H";
+                flag=0;
+            }else
+                letter[0]="h";
+
+        }else if(Arrays.equals(i,snap)){
+            if (flag==1){
+                letter[0]="I";
+                flag=0;
+            }else
+                letter[0]="i";
+
+        }else if(Arrays.equals(j, snap)){
+            if (flag==1){
+                letter[0]="J";
+                flag=0;
+            }else
+                letter[0]="j";
+
+        }else if(Arrays.equals(k, snap)){
+            if (flag==1){
+                letter[0]="K";
+                flag=0;
+            }else
+                letter[0]="k";
+
+        }else if(Arrays.equals(l, snap)){
+            if (flag==1){
+                letter[0]="L";
+                flag=0;
+            }else
+                letter[0]="l";
+
+        }else if(Arrays.equals(m, snap)){
+            if (flag==1){
+                letter[0]="M";
+                flag=0;
+            }else
+                letter[0]="m";
+
+        }else if(Arrays.equals(n, snap)){
+            if (flag==1){
+                letter[0]="N";
+                flag=0;
+            }else
+                letter[0]="n";
+
+        }else if(Arrays.equals(o, snap)){
+            if (flag==1){
+                letter[0]="O";
+                flag=0;
+            }else
+                letter[0]="o";
+
+        }else if(Arrays.equals(p, snap)){
+            if (flag==1){
+                letter[0]="P";
+                flag=0;
+            }else
+                letter[0]="p";
+
+        }else if(Arrays.equals(q, snap)){
+            if (flag==1){
+                letter[0]="Q";
+                flag=0;
+            }else
+                letter[0]="q";
+
+        }else if(Arrays.equals(r, snap)){
+            if (flag==1){
+                letter[0]="R";
+                flag=0;
+            }else
+                letter[0]="r";
+
+        }else if(Arrays.equals(s, snap)){
+            if (flag==1){
+                letter[0]="S";
+                flag=0;
+            }else
+                letter[0]="s";
+
+        }else if(Arrays.equals(t, snap)){
+            if (flag==1){
+                letter[0]="T";
+                flag=0;
+            }else
+                letter[0]="t";
+
+        }else if(Arrays.equals(u, snap)){
+            if (flag==1){
+                letter[0]="U";
+                flag=0;
+            }else
+                letter[0]="u";
+
+        }else if(Arrays.equals(v, snap)){
+            if (flag==1){
+                letter[0]="V";
+                flag=0;
+            }else
+                letter[0]="v";
+
+        }else if(Arrays.equals(w, snap)){
+            if (flag==1){
+                letter[0]="W";
+                flag=0;
+            }else
+                letter[0]="w";
+
+        }else if(Arrays.equals(x, snap)){
+            if (flag==1){
+                letter[0]="X";
+                flag=0;
+            }else
+                letter[0]="x";
+
+        }else if(Arrays.equals(y, snap)){
+            if (flag==1){
+                letter[0]="Y";
+            }else
+                letter[0]="y";
+
+        }else if(Arrays.equals(z, snap)){
+            if (flag==1){
+                letter[0]="Z";
+                flag=0;
+            }else
+                letter[0]="z";
+
+        }else if(Arrays.equals(cedilha, snap)){
+            letter[0]="ç";
+        }else if(Arrays.equals(espaco, snap)){
+            letter[0]=" ";
+        }else if(Arrays.equals(ag, snap)){
+            letter[0]="á";
+        }else if(Arrays.equals(eg, snap)){
+            letter[0]="é";
+        }else if(Arrays.equals(ig, snap)){
+            letter[0]="í";
+        }else if(Arrays.equals(og, snap)){
+            letter[0]="ó";
+        }else if(Arrays.equals(ug, snap)){
+            letter[0]="ú";
+        }else if(Arrays.equals(acr,snap)){
+            letter[0]="à";
+        }else if(Arrays.equals(ecr,snap)){
+            letter[0]="è";
+        }else if(Arrays.equals(icr, snap)){
+            letter[0]="ì";
+        }else if(Arrays.equals(ocr, snap)){
+            letter[0]="ò";
+        }else if(Arrays.equals(ucr, snap)){
+            letter[0]="ù";
+        }else if(Arrays.equals(ac, snap)){
+            letter[0]="â";
+        }else if(Arrays.equals(ec, snap)){
+            letter[0]="ê";
+        }else if(Arrays.equals(oc, snap)){
+            letter[0]="ô";
+        }else if(Arrays.equals(at, snap)){
+            letter[0]="ã";
+        }else if(Arrays.equals(ot, snap)){
+            letter[0]=""; //verificar
+        }else if(Arrays.equals(virg, snap)){
+            letter[0]=" == ";
+        }else if(Arrays.equals(doisp, snap)){
+            letter[0]=":";
+        }else if(Arrays.equals(ponto, snap)){
+            letter[0]=".";
+        }else if(Arrays.equals(inte, snap)){
+            letter[0]="?";
+        }else if(Arrays.equals(excla, snap)){
+            letter[0]="!";
+        }else if(Arrays.equals(pontvirg, snap)){
+            letter[0]=";";
+        }else if(Arrays.equals(hif, snap)){
+            letter[0]="-";
+        }else if(Arrays.equals(abP, snap)){
+            letter[0]="(";
+        }else if(Arrays.equals(feP, snap)){
+            letter[0]="){";
+        }else if(Arrays.equals(mais, snap)){
+            letter[0]="+";
+        }else if(Arrays.equals(aster, snap)){
+            letter[0]="*";
+        }else if(Arrays.equals(divisao, snap)){
+            letter[0]="/";
+        }else if(Arrays.equals(igual, snap)){
+            letter[0]="=";
+
+        }else if(Arrays.equals(maiuscula, snap)){
+            flag=1;
+            letter[0]="";
+        }else {
+            letter[0] = "#";
+        }
+
+
+        letter[1]= String.valueOf(flag);
+        return letter;
+
+    }
+}
